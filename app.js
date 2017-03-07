5ccb766fa40c6b37af96bfa7e3cfd963ffc8b125
Sarah Herr <sarah.herr@hotmail.com>
2017-03-07 10:56:10 -0500

    up to git collaboration

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 const express = require("express");
-// const morgan = require("morgan"); // isnt working right now, will ask fellow in AM
 const app = express(); // creates an instance of an express application
-// const volleyball = require("vollyball"); // isnt working right now, will ask fellow in AM
+const volleyball = require("volleyball");
 
-app.use(function(req, res, next) {
-    next();
-})
+app.use(volleyball);
+// app.use(function(req, res, next) {
+//     next();
+// })
 
 app.get("/", function(req, res) {
-    res.send("<h1>Welcome to Twitter.js</h1>");
+    res.send("<h1>Welcome to Twitter</h1>");
     // console.log(req.method, req.path);
 });
 
 app.get("/news", function(req, res) {
     res.send("<h1>Latest News</h1>");
 });
+console.log('abc');
 
 app.get("/is-anybody-in-there", function(req, res) {
-    res.send("<h3><em>IS ANYBODY IN THERE?</em></h3>");
+    res.send("<h3><em>Hello?</em></h3><h4><em>Hello?</em></h4><h5><em>Hello?</em></h5>");
     // console.log(req.method, req.path);
 })
 
@@ -26,6 +27,7 @@ app.get("/modernism", function(req, res) {
 })
 
 app.post("/modernism", function(req, res) {
+    res.send('POST request to homepage');
     // console.log(req.method, req.path);
 })
 
